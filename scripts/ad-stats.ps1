50f633ffcf2f6c9a427713b86fc5dd2db1c11e3e
Bjarke Falch Sørensen <bs@wasd.dk>
2026-03-25 17:08:33 +0100

    Use the right verbs

--- scripts/ad-stats.ps1
+++ scripts/ad-stats.ps1
@@ -1,5 +1,5 @@
-$expired = (get-aduser -filter * -properties *|where {$_.Enabled -and $_.PasswordExpired} |measure).Count
-$locked = (get-aduser -filter * -properties *|where {$_.Enabled -and $_.LockedOut} |measure).Count
+$expired = (get-aduser -filter * -properties *|Where-Object {$_.Enabled -and $_.PasswordExpired} |Measure-Object).Count
+$locked = (get-aduser -filter * -properties *|Where-Object {$_.Enabled -and $_.LockedOut} |Measure-Object).Count
 $date = get-date
 "$date, $expired,$locked" | Out-File data/ad-stats.txt -Append
 write-host $date, $expired,$locked
