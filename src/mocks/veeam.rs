c9e8ffb39a2a6bc7dbfb33ef2186e956b5e11c13
Bjarke Falch Sørensen <bs@wasd.dk>
2026-03-25 16:10:50 +0100

    Make a mock visibly mocking

--- src/mocks/veeam.rs
+++ src/mocks/veeam.rs
@@ -3,10 +3,10 @@ use serde_json::{json, Value};
 pub fn payload() -> Value {
     json!({
         "_mock": true,
-        "green":  5,
+        "green":  1,
         "yellow": 1,
-        "orange": 0,
-        "red":    0,
-        "grey":   0
+        "orange": 1,
+        "red":    1,
+        "grey":   1
     })
 }
